2ec737bef172de548ee82a639f349bef0f270e9f
rahmat <rahmatnazali95@gmail.com>
2019-04-24 08:51:23 +0700

    working snippets of file traverse and copying (not using shutil.copytree because we need to manipulate each file)

--- snippets/05_file traversing and copy_pathlib.py
+++ snippets/05_file traversing and copy_pathlib.py
@@ -2,11 +2,13 @@
 import os
 import errno
 from shutil import copyfile
+from pathlib import Path
 
 # todo: rename filename
 # todo: rename content
 
 # os crawler
+from numpy.distutils.conv_template import paren_repl
 
 output_root = '../output'
 target = 'to_here'
@@ -19,21 +21,27 @@ for path, directory, files in os.walk('../scaffolder/default_folder'):
 
     for file in files:
         # get the path, etc
-        file_source_path = os.path.join(path, file)
+        # file_source_path = os.path.join(path, file)
+        file_source_path = Path(path, file)
+
         if root_directory == 'default_folder':
-            file_dest_path = os.path.join(output_root, target, file)
+            # file_dest_path = os.path.join(output_root, target, file)
+            file_dest_path = Path(output_root, target, file)
         else:
-            file_dest_path = os.path.join(output_root, target, root_directory, file)
+            # file_dest_path = os.path.join(output_root, target, root_directory, file)
+            file_dest_path = Path(output_root, target, root_directory, file)
 
         print('source:', file_source_path)
         print('dest:', file_dest_path)
 
+
         # todo: process the file
 
         # make sure that directory is exist
-        if not os.path.exists(os.path.dirname(file_dest_path)):
+        if not file_dest_path.exists():
             try:
                 os.makedirs(os.path.dirname(file_dest_path))
+                # file_dest_path.mkdir(file_dest_path.parent, exist_ok=True)
             except OSError as exc: # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
