400f622a9d0b62aa6131a9e82acef89d895c99f7
rahmat <rahmatnazali95@gmail.com>
2019-04-25 18:58:48 +0700

    result example

---
+++ output/path/to/your/prefered/folder/ScaffoldedClass.php
@@ -0,0 +1,4 @@
+class ScaffoldedClass {
+    public $one = "";
+    public scaffolded_variable = "this variable name is decorated";
+}
---
+++ output/path/to/your/prefered/folder/scaffolded_file_name.py
@@ -0,0 +1,13 @@
+def scaffolded_function_name():
+    variable_from_b_1 = None
+    variable_from_b_2 = True
+    variable_from_b_3 = True or False
+
+    variable_from_c_1 = (1, 2)
+    variable_from_c_2 = [1, 2]
+    variable_from_c_3 = (1, 2, 3, 4)
+
+    variable_from_d_1 = {}
+    variable_from_d_2 = {'a': 'a', 'b': 'b'}
+    variable_from_d_3 = {'a':{'a' : 'a'}, 'b':{'b' : 'b'}}
+
